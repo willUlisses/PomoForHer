03f40b1411f90c307f6189daffb6511a1166b796
willUlisses <williamcavalcante06@gmail.com>
2026-02-14 13:36:52 -0300

    First commit of the project

---
+++ Pomo-for-her/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Options from "./Components/Options"
+
+function App() {
+
+  return (
+    <main className='bg-violet-400 h-screen flex items-center justify-center'>
+      <Options></Options>
+    </main>
+  )
+}
+
+export default App
---
+++ Pomo-for-her/src/Components/Options.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+
+
+const Options = () => {
+    const [activeOption, setActiveOption] = useState<string>("15/2")
+
+    const options = ["15/2", "30/5", "60/10"];
+
+    return (
+       <ul className="w-fit text-2xl grid grid-cols-3 text-white font-medium bg-violet-900 overflow-hidden rounded-xl divide-zinc-100 shadow-2xl">
+            {
+                options.map((time) => {
+                    const isActive = (activeOption === time);
+                    return (
+                        <li 
+                        key={time}
+                        onClick={() => {setActiveOption(time)}}
+                        className={`
+                        px-10 py-3 cursor-pointer transition-all duration-200
+                        ${isActive ? 'bg-violet-600' : 'hover:bg-white/5'}
+                        `}
+                        >
+                            {time}
+                        </li>
+                    );
+                })
+            }
+       </ul> 
+    )
+}
+
+export default Options;
