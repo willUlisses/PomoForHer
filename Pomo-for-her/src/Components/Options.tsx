cb700ec6788b31c6e83aa530d5f040b9e01dbaea
willUlisses <williamcavalcante06@gmail.com>
2026-02-15 13:00:01 -0300

    Finished Start Stop Button

--- Pomo-for-her/src/Components/Options.tsx
+++ Pomo-for-her/src/Components/Options.tsx
@@ -2,25 +2,25 @@ import { useState } from "react";
 
 
 const Options = () => {
-    const [activeOption, setActiveOption] = useState<string>("15/2")
+    const [activeOption, setActiveOption] = useState<string>("25/5")
 
-    const options = ["15/2", "30/5", "60/10"];
+    const options = ["25/5", "50/10", "100/20"];
 
     return (
        <ul className="w-fit text-2xl grid grid-cols-3 text-white font-medium bg-violet-900 overflow-hidden rounded-xl divide-zinc-100 shadow-2xl">
             {
-                options.map((time) => {
-                    const isActive = (activeOption === time);
+                options.map((timeOption) => {
+                    const isActive = (activeOption === timeOption);
                     return (
                         <li 
-                        key={time}
-                        onClick={() => {setActiveOption(time)}}
+                        key={timeOption}
+                        onClick={() => {setActiveOption(timeOption)}}
                         className={`
                         px-10 py-3 cursor-pointer transition-all duration-200
                         ${isActive ? 'bg-violet-600' : 'hover:bg-white/5'}
                         `}
                         >
-                            {time}
+                            {timeOption}
                         </li>
                     );
                 })
--- Pomo-for-her/src/Components/Timer.tsx
+++ Pomo-for-her/src/Components/Timer.tsx
@@ -9,33 +9,30 @@ function timeFormat(timeInSeconds: number): string {
     return minutesUnit + ":" + secondsUnit;
 }
 
-function onStartStopClick(isPlaying:boolean, time:number) {
-    if (isPlaying) {
-        
-    }
-}
-
 const Timer = () => {
     const [workTime, setWorkTime] = useState<number>(900)
     const [isPlaying, setIsPlaying] = useState<boolean>(false)
 
     useEffect(() => {
         if (workTime <= 0) return;
-
-        const intervalId = setInterval(() => {setWorkTime(tempoAtual => tempoAtual - 1)}, 1000)
-        return () => clearInterval(intervalId);
-    }, [workTime]);
+        
+        if (isPlaying) {
+            const timeDecreaseInterval = setInterval(() => {setWorkTime(tempoAtual => tempoAtual - 1)}, 1000)
+            return () => clearInterval(timeDecreaseInterval);
+        } 
+        return;
+    }, [workTime, isPlaying]);
 
     return (
-        <div className="flex flex-col gap-6 px-4 py-4 bg-violet-950 rounded-xl">
+        <div className="flex flex-col gap-6 px-4 py-4 bg-violet-950 rounded-xl shadow-2xl">
             <Options></Options>
-            <h1 className="font-mono text-9xl text-white self-center">
+            <h1 className="font-mono text-9xl text-white self-center text-shadow-lg">
                 {workTime > 0 ? timeFormat(workTime) : "Tempo Esgotado!"}
             </h1>
             <button 
             type="button"
             onClick={() =>{ setIsPlaying(!isPlaying)}}
-            className="text-2xl text-white bg-violet-500 rounded-lg font-medium py-2">
+            className="text-2xl text-white bg-violet-500 rounded-lg font-medium py-2 active:scale-98">
                 {isPlaying ? "Stop" : "Start"}
             </button>
         </div>
