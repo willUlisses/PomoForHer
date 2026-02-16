1da6db32dea5d9d6032b7509c6f2dfcde14df7e4
willUlisses <williamcavalcante06@gmail.com>
2026-02-16 16:24:49 -0300

    refactor: changed logic of calculating total time left in seconds into a method

--- Pomo-for-her/src/Components/Timer.tsx
+++ Pomo-for-her/src/Components/Timer.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import Options from "./Options";
 
-
-
 function timeFormat(timeInSeconds: number): string {
     const minutesUnit: number = Math.floor(timeInSeconds / 60);
     const secondsUnit: number = Math.floor(timeInSeconds % 60);
@@ -10,38 +8,46 @@ function timeFormat(timeInSeconds: number): string {
     return `${minutesUnit.toString().padStart(2, "0")}:${secondsUnit.toString().padStart(2, "0")}`;
 }
 
-const Timer = () => {
-    const [activeOption, setActiveOption] = useState<string>("25/5")
-    const [workTime, setWorkTime] = useState<number>(1500)
-    const [isPlaying, setIsPlaying] = useState<boolean>(false)
+function takeTimeFromOptionString(option: string, index: number): number {
+        const timeInMinutes = option.split("/")[index]; // pego o tempo em minutos total (a depender do index o de work ou de restTime)
+        const totalTimeLeftInSeconds = parseInt(timeInMinutes) * 60; 
 
-    const handleOptionSwitch = (option: string) => {
+        return totalTimeLeftInSeconds;
+}
+
+const Timer = () => {
+    const [activeOption, setActiveOption] = useState<string>("25/5");
+    const [timeLeft, setTimeLeft] = useState<number>(1500);
+    const [isRestTimeOn, setIsRestTimeOn] = useState<boolean>(false);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+
+    const handleMinutesOptionSwitch = (option: string) => {
         setActiveOption(option);
         setIsPlaying(false);
 
-        const [minuteString] = option.split("/"); // pego o tempo em minutos total
-        const timeInSeconds = parseInt(minuteString) * 60; // transformo o tempo de minutos pro total dele em segundos
-
-        setWorkTime(timeInSeconds); // mudo o worktime pro tempo total em segundos e depois ele lida com a formatação
+        const timeInSeconds:number = takeTimeFromOptionString(option, 0); 
+        setTimeLeft(timeInSeconds); // mudo o timeLeft pro tempo total em segundos e depois ele lida com a formatação
     }
 
     useEffect(() => {
-        if (workTime <= 0) {
+        if (timeLeft <= 0) {
             setIsPlaying(false);
             return
         };
-        
+
+
         if (isPlaying) {
-            const timeDecreaseInterval = setInterval(() => {setWorkTime(tempoAtual => tempoAtual - 1)}, 1000)
+            const timeDecreaseInterval = setInterval(() => {setTimeLeft(tempoAtual => tempoAtual - 1)}, 1000)
             return () => clearInterval(timeDecreaseInterval);
         } 
-    }, [workTime, isPlaying]);
+    }, [timeLeft, isPlaying, isRestTimeOn]);
 
     return (
             <div className="flex flex-col gap-6 px-4 py-4 bg-violet-950 rounded-xl shadow-2xl text-white">
-                <Options activeOption={activeOption} onOptionSelect={handleOptionSwitch}></Options>
+                <Options activeOption={activeOption} onOptionSelect={handleMinutesOptionSwitch}></Options>
                 <h1 className="font-extrabold tracking-wider text-9xl self-center text-shadow-lg my-10">
-                    {timeFormat(workTime)}
+                    {timeFormat(timeLeft)}
                 </h1>
                 <button 
                 type="button"
