914e3e05f90b5430cb952f07a09f9d07b4591f72
willUlisses <williamcavalcante06@gmail.com>
2026-02-17 10:43:25 -0300

    feat: added a toast to give a warning when the rest time starts

--- Pomo-for-her/src/Components/RestToast.tsx
+++ Pomo-for-her/src/Components/RestToast.tsx
@@ -1,19 +1,21 @@
+import type { ReactNode } from "react";
+
 interface ToastProps {
-  message: string;
+  children: ReactNode;
   visible: boolean;
 }
 
-const RestToast = ({ message, visible } : ToastProps) => {
+const RestToast = ({ children, visible }: ToastProps) => {
   return (
     <div
       className={`
         absolute -top-16 left-1/2 -translate-x-1/2 
-        bg-emerald-500 text-white px-6 py-2 rounded-full shadow-xl font-bold
+        bg-violet-900 text-white text-lg px-6 p-3 rounded-full shadow-xl font-bold tracking-wider
         transition-all duration-500 ease-in-out z-50 whitespace-nowrap
         ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4 pointer-events-none'}
       `}
     >
-      {message}
+      {children}
     </div>
   );
 };
