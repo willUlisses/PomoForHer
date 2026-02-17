30ecd15eab68cbcaeea4c57c9b5e9983199a3381
willUlisses <williamcavalcante06@gmail.com>
2026-02-17 10:15:02 -0300

    Finished work time to rest time transition

--- Pomo-for-her/src/Components/Timer.tsx
+++ Pomo-for-her/src/Components/Timer.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import Options from "./Options";
 
+
+type TimerMode = "WORK" | "REST";
+
 function timeFormat(timeInSeconds: number): string {
     const minutesUnit: number = Math.floor(timeInSeconds / 60);
     const secondsUnit: number = Math.floor(timeInSeconds % 60);
@@ -17,44 +20,77 @@ function takeTimeFromOptionString(option: string, index: number): number {
 
 const Timer = () => {
     const [activeOption, setActiveOption] = useState<string>("25/5");
-    const [timeLeft, setTimeLeft] = useState<number>(1500);
-    const [isRestTimeOn, setIsRestTimeOn] = useState<boolean>(false);
+    const [durations, setDurations] = useState({work: 1500, rest: 300})
+    const [timeLeft, setTimeLeft] = useState<number>(2);
+    const [timeMode, setTimeMode] = useState<TimerMode>("WORK");
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
 
     const handleMinutesOptionSwitch = (option: string) => {
         setActiveOption(option);
-        setIsPlaying(false);
+        setIsPlaying(false); // vai começar com o timer pausado
+        setTimeMode("WORK"); // quando ela escolher alguma opção vai estar no workMode automaticamente
 
-        const timeInSeconds:number = takeTimeFromOptionString(option, 0); 
-        setTimeLeft(timeInSeconds); // mudo o timeLeft pro tempo total em segundos e depois ele lida com a formatação
+        const workTimeInSeconds:number = takeTimeFromOptionString(option, 0); // pega os segundos totais do workTime
+        const restTimeinSeconds:number = takeTimeFromOptionString(option, 1); // pega os segundos totais do restTime
+
+        setDurations({work: workTimeInSeconds, rest: restTimeinSeconds}); // atualiza as durations de work e rest
+        setTimeLeft(workTimeInSeconds); // mudo o timeLeft pro tempo total em segundos e depois ele lida com a formatação
+    }
+
+    function switchTimeMode() {
+        if (timeMode === "WORK") {
+            setTimeMode("REST");
+            setTimeLeft(durations.rest);
+            
+        } else {
+            setTimeMode("WORK");
+            setTimeLeft(durations.work);
+            setIsPlaying(false);
+        }
     }
 
     useEffect(() => {
-        if (timeLeft <= 0) {
-            setIsPlaying(false);
-            return
-        };
+        let intervalId: number;
+
+        if (isPlaying && timeLeft > 0) {
+            intervalId = setInterval(() => {
+                setTimeLeft((tempoAtual) => tempoAtual - 1);
+            }, 1000);
+        }
+        else if (timeLeft === 0 && isPlaying) {
+            switchTimeMode();
+        }
+
+        return () => clearInterval(intervalId);
+        } , [timeLeft, isPlaying, timeMode, durations]);
 
 
-        if (isPlaying) {
-            const timeDecreaseInterval = setInterval(() => {setTimeLeft(tempoAtual => tempoAtual - 1)}, 1000)
-            return () => clearInterval(timeDecreaseInterval);
-        } 
-    }, [timeLeft, isPlaying]);
+    const isWorkModeOn = (timeMode === "WORK");
 
     return (
             <div className="flex flex-col gap-6 px-4 py-4 bg-violet-950 rounded-xl shadow-2xl text-white">
+
                 <Options activeOption={activeOption} onOptionSelect={handleMinutesOptionSwitch}></Options>
-                <h1 className="font-extrabold tracking-wider text-9xl self-center text-shadow-lg my-10">
-                    {timeFormat(timeLeft)}
-                </h1>
+
+                <div className="text-center">
+                    <p className="text-zinc-400 text-xl font-bold tracking-widest my-5">
+                        {isWorkModeOn ? 'STUDY TIME' : 'BREAK TIME'}
+                    </p>
+
+                    <h1 className="font-extrabold tracking-wider text-9xl self-center text-shadow-lg my-5">
+                        {timeFormat(timeLeft)}
+                    </h1>
+                </div>
+                
                 <button 
                 type="button"
-                onClick={() =>{ setIsPlaying(!isPlaying)}}
-                className="text-2xl bg-violet-500 rounded-lg font-medium py-2 active:scale-98">
-                    {isPlaying ? "Stop" : "Start"}
-                </button>
+                onClick={() => setIsPlaying(!isPlaying)}
+                className={`text-xl text-white bg-violet-500 rounded-lg font-bold py-3 px-8 w-full active:scale-95 transition-all shadow-lg hover:brightness-110`}
+            >
+                {isPlaying ? "PAUSAR" :  "INICIAR"}
+            </button>
+
             </div>
     )
 }
