8dc0735b320ebb6258b75d89448f963ad4527185
willUlisses <williamcavalcante06@gmail.com>
2026-02-17 10:45:14 -0300

    refactor: turned Options component into a responsive design for mobile screens

--- Pomo-for-her/src/Components/Options.tsx
+++ Pomo-for-her/src/Components/Options.tsx
@@ -7,7 +7,9 @@ const Options = ({activeOption, onOptionSelect} : OptionsProps) => {
     const options = ["25/5", "50/10", "100/20"];
 
     return (
-       <ul className="w-fit text-2xl font-bold tracking-wider grid grid-cols-3 text-white bg-violet-900 overflow-hidden rounded-xl divide-zinc-100 shadow-2xl">
+       <ul className="w-full sm:w-fit text-lg md:text-2xl font-bold 
+       tracking-wider grid grid-cols-3 text-white bg-violet-900 
+       overflow-hidden rounded-xl shadow-2xl">
             {
                 options.map((timeOption) => {
                     const isActive = (activeOption === timeOption);
@@ -16,7 +18,7 @@ const Options = ({activeOption, onOptionSelect} : OptionsProps) => {
                         key={timeOption}
                         onClick={() => {onOptionSelect(timeOption)}}
                         className={`
-                        px-10 py-3 cursor-pointer transition-all duration-200
+                        px-4 md:px-10 py-3 text-center cursor-pointer transition-all duration-200
                         ${isActive ? 'bg-violet-600' : 'hover:bg-white/5'}
                         `}
                         >
