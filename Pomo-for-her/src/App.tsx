62bb28fd77ce243c864d54890b9c7858bc9320ab
Vercel <vercel[bot]@users.noreply.github.com>
2026-02-20 01:37:18 +0000

    Enable Vercel Speed Insights on your project
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- Pomo-for-her/src/App.tsx
+++ Pomo-for-her/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Timer from './Components/Timer'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
 
   return (
     <main className='bg-violet-400 h-screen flex items-center justify-center'>
       <Timer></Timer>
+      <SpeedInsights />
     </main>
   )
 }
